4c5d33a73a5d9c827f3f88a985ad4d8bb6b52d84
fmoorhof <fmoorhof@ipb-halle.de>
2023-08-07 13:43:22 +0200

    fix: exit code 5: pytest naming convention

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -4,23 +4,23 @@ from src.logic import Stack
 
 
 @pytest.fixture(name="stack_inst")
-def stack_fixture_test():
+def stack_fixture():
     return Stack()
 
 
-def constructor_test(stack_inst):
+def test_constructor(stack_inst):
     assert isinstance(stack_inst, Stack)
     assert len(stack_inst) == 0
 
 
-def push_test(stack_inst):
+def test_push(stack_inst):
     stack_inst.push(3)
     assert len(stack_inst) == 1
     stack_inst.push(5)
     assert len(stack_inst) == 2
 
 
-def pop_test(stack_inst):
+def test_pop(stack_inst):
     stack_inst.push("abc")
     stack_inst.push("def")
     assert stack_inst.pop() == "def"
