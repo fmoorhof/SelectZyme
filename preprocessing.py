50744cf4ca5d4703bacee64c37db3911f6929673
fmoorhof <fmoorhof@ipb-halle.de>
2024-01-22 17:49:35 +0100

    feat: preprocessing functions, tests: add preprocessing tests

--- preprocessing.py
+++ preprocessing.py
@@ -2,52 +2,96 @@
 This file provides basic functionalites for preprocessing sequences e.g. file parsing
 """
 import logging
-from itertools import groupby
 
+import pandas as pd
+
+
+class Parsing():
+    """This class should assist in the parsing of the data."""
+    def parse_fasta(filepath):
+        """Parse a fasta file and return a list for the headers and one for the sequences.
+        params: filepath: path to the fasta file
+        return: headers: list of headers
+        return: sequences: list of sequences"""
+        headers = []
+        sequences = []
+        with open(filepath, 'r') as file:
+            sequence = ""
+            for line in file:
+                if line.startswith('>'):
+                    if sequence != "":
+                        sequences.append(sequence)
+                        sequence = ""
+                    headers.append(line.strip())
+                else:
+                    sequence += line.strip()
+            sequences.append(sequence)  # Append the last sequence
+        return headers, sequences
+    
+    def parse_tsv(filepath):
+        """Parse a tsv file and return a dataframe.
+        params: filepath: path to the tsv file
+        return: df: dataframe containing the sequences"""
+        df = pd.read_csv(filepath, sep='\t')
+        return df    
+    
 
 class Preprocessing:
-    """
-    This class should assist in the preprocessing of the data.
-    e.g. parsing the fasta file.
-    """
-    def read_fasta(filepath):
-        """
-        Iteratively returns the entries from a fasta file.
-        
-        Parameters
-        ----------
-        file : str
-            the fasta file
-        
-        Yields
-        ------
-        output : tuple
-            a single entry in the fasta file (header, sequence)
-        """
-        is_header = lambda x: x.startswith('>')
-        compress  = lambda x: ''.join(_.strip() for _ in x)
-        reader    = iter(groupby(open(filepath), is_header))
-        reader    = iter(groupby(open(filepath), is_header)) if next(reader)[0] else reader
-        for key, group in reader:
-            if key:
-                for header in group:
-                    header = header[1:].strip()
-            else:
-                sequence = compress(group)
-                if sequence != '':
-                    yield header, sequence
+    """This class should assist in the preprocessing of the data."""
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+        self.length = df.shape[0]  # original length of the dataframe
 
+    def remove_long_sequenes(self) -> pd.DataFrame:
+        """
+        This function removes too long sequences from the dataset. Sequences > 1024 amino acids cause the esm embedding to fail.
+        params: df: dataframe containing the sequences
+        return: df: dataframe containing only sequences with a length <= 1024 amino acids
+        """
+        df = self.df[self.df['Sequence'].str.len() <= 1024]
+        df.drop_duplicates(inplace=True)
+        df.reset_index(drop=True, inplace=True)  # todo: with removed index log which sequences were excluded
+        logging.info(f'{self.length-df.shape[0]} sequences were excluded because of exaggerated size (>=1024 amino acids)')
+        return df
+
+    def remove_sequences_without_Metheonin(self) -> pd.DataFrame:
+        """
+        This function removes sequences without a Methionine at the beginning.
+        params: df: dataframe containing the sequences
+        return: df: dataframe containing only sequences with a Methionine at the beginning
+        """
+        df = self.df[self.df['Sequence'].str.startswith('M')]
+        df.reset_index(drop=True, inplace=True)
+        logging.info(f'{self.length-df.shape[0]} sequences were excluded because of missing Methionins.')
+        return df
+    
+    def remove_sequences_with_undertermined_amino_acids(self) -> pd.DataFrame:
+        """
+        This function removes sequences with undertermined amino acids 'X'.
+        params: df: dataframe containing the sequences
+        return: df: dataframe containing only sequences without undertermined amino acids
+        """
+        df = self.df[~self.df['Sequence'].str.contains('X')]
+        df.reset_index(drop=True, inplace=True)
+        logging.info(f'{self.length-df.shape[0]} sequences were excluded because of undertermined amino acids.')
+        return df
 
 
 
 if __name__=='__main__':
 
-    # example dataset from the paper
+    # example datasets
     fasta_file = 'tests/head_10.fasta'
+    fasta_file = 'tests/head_10.tsv'
 
-    # todo: re-write: this is super ugly -> parse as df directly
-    headers = []
-    sequences = []
-    for h, s in Preprocessing.read_fasta(fasta_file):
-        headers.append(h)
-        sequences.append(s)
+    if fasta_file.endswith('.fasta'):
+        headers, sequences = Parsing.parse_fasta(fasta_file)
+        df = pd.DataFrame({'Header': headers, 'Sequence': sequences})
+    else:
+        df = Parsing.parse_tsv(fasta_file)
+
+    # df needs to contain a column 'Sequence' with the sequences
+    pp = Preprocessing(df)
+    df = pp.remove_long_sequenes(df)
+    df = pp.remove_sequences_without_Metheonin(df)
+    df = pp.remove_sequences_with_undertermined_amino_acids(df)
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,17 +1,80 @@
 import pytest
+import pandas as pd
 
-from preprocessing import read_fasta
+from preprocessing import Parsing
+from preprocessing import Preprocessing
 
 
-class TestPreprocessing:
-    """Test the preprocessing function."""
-    def test_reading(fasta_file = 'head_10.fasta'):
-        """Test reading of a fasta file that consists of headers and sequences."""
-        headers = []
-        sequences = []
-        for h, s in read_fasta(fasta_file):
-            headers.append(h)
-            sequences.append(s)
+def test_parse_fasta(fasta_file = 'tests/head_10.fasta'):
+    """Test parsing of a fasta file that consists of headers and sequences."""
+    headers, sequences = Parsing.parse_fasta(fasta_file)
 
-        assert headers is not []
-        assert sequences is not []
+    assert headers is not []
+    assert sequences is not []
+
+def test_parse_tsv(tsv_file = 'tests/head_10.tsv'):
+    """Test parsing of a tsv file that consists of headers and sequences."""
+    df = Parsing.parse_tsv(tsv_file)
+
+    assert df is not None
+
+
+class TestPreprocessingFASTA:
+    """Test the preprocessing functions."""
+    def setup_method(self, method):
+        # Replace this with the code to create your DataFrame
+        headers, sequences = Parsing.parse_fasta('tests/head_10.fasta')
+        self.df = pd.DataFrame({'Header': headers, 'Sequence': sequences})
+        self.length = self.df.shape[0]  # original length of the dataframe
+        self.preprocessing = Preprocessing(self.df)  # instantiate the Preprocessing class
+
+    def test_remove_long_sequences(self):
+        """Test the removal of too long sequences."""
+        df = self.preprocessing.remove_long_sequenes()
+
+        assert df is not None
+        assert df.shape[0] != self.length
+
+    def test_remove_sequences_without_Metheonin(self):
+        """Test the removal of sequences without Methionine."""
+        df = self.preprocessing.remove_sequences_without_Metheonin()
+
+        assert df is not None
+        assert df.shape[0] != self.length
+
+    def test_remove_sequences_with_undertermined_amino_acids(self): 
+        """Test the removal of sequences with undertermined amino acids."""
+        df = self.preprocessing.remove_sequences_with_undertermined_amino_acids()
+
+        assert df is not None
+        assert df.shape[0] != self.length
+
+
+class TestPreprocessingTSV:
+    """Test the preprocessing functions."""
+    def setup_method(self, method):
+        # Replace this with the code to create your DataFrame
+        self.df = Parsing.parse_tsv('tests/head_10.tsv')
+        self.length = self.df.shape[0]  # original length of the dataframe
+        self.preprocessing = Preprocessing(self.df)  # instantiate the Preprocessing class
+
+    def test_remove_long_sequences(self):
+        """Test the removal of too long sequences."""
+        df = self.preprocessing.remove_long_sequenes()
+
+        assert df is not None
+        assert df.shape[0] != self.length
+
+    def test_remove_sequences_without_Metheonin(self):
+        """Test the removal of sequences without Methionine."""
+        df = self.preprocessing.remove_sequences_without_Metheonin()
+
+        assert df is not None
+        assert df.shape[0] != self.length
+
+    def test_remove_sequences_with_undertermined_amino_acids(self): 
+        """Test the removal of sequences with undertermined amino acids."""
+        df = self.preprocessing.remove_sequences_with_undertermined_amino_acids()
+
+        assert df is not None
+        assert df.shape[0] != self.length
