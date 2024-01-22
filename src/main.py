73e7d93ab7b3752524849d83a5001100e8df4f06
fmoorhof <fmoorhof@ipb-halle.de>
2024-01-22 20:30:33 +0100

    style: objectification, tests: add new tests

--- src/main.py
+++ src/main.py
@@ -1,23 +1,46 @@
-import click
-from src.logic import Stack
+import logging
+
+import pandas as pd
+from qdrant_client import QdrantClient, models
 
 
-@click.command()
-@click.option(
-    "--location",
-    help="This specifies the location you want to know the time. For example, Lagos or London",
+from preprocessing import Parsing
+from preprocessing import Preprocessing
+from embed import *
+
+
+logging.basicConfig(
+    format="%(levelname)-8s| %(module)s.%(funcName)s: %(message)s", level=logging.DEBUG
 )
-@click.option(
-    "--zone",
-    help="The timezone information you need. Ensure it is properly capitalized, for example CET or WAT",
-)
-def main(location, zone):
-    st = Stack()
-    if location:
-        st.push(location)
-    if zone:
-        st.pop(zone)
+
+
+def db_creation(df, collection_name: str):
+    collections_info = qdrant.get_collections()
+    if collection_name not in str(collections_info):  # todo: implement this nicely: access the 'name' field of the object
+        embeddings = gen_embedding(df['Sequence'].tolist(), device='cuda')
+        create_vector_db_collection(df, embeddings, collection_name=collection_name)
+    else:
+        df = load_collection_from_vector_db(collection_name='test_collection')
+
+
+def main(input_file: str, project_name: str):
+
+    if input_file.endswith('.fasta'):
+        headers, sequences = Parsing.parse_fasta(input_file)
+        df = pd.DataFrame({'Header': headers, 'Sequence': sequences})
+    else:
+        df = Parsing.parse_tsv(input_file)
+
+    # df needs to contain a column 'Sequence' with the sequences
+    pp = Preprocessing(df)
+    df = pp.remove_long_sequenes(df)
+    df = pp.remove_sequences_without_Metheonin(df)
+    df = pp.remove_sequences_with_undertermined_amino_acids(df)
+
+    # Create a collection in Qdrant DB with embedded sequences
+    db_creation(df, collection_name=project_name)
+
 
 
 if __name__ == "__main__":
-    main()
+    main(input_file='tests/head_10.tsv', project_name='test_project')
--- tests/test_embed.py
+++ tests/test_embed.py
@@ -1,17 +1,66 @@
 import pytest
+from qdrant_client import QdrantClient
 
-from embed import gen_embedding
+import embed
+from preprocessing import Parsing
+from preprocessing import Preprocessing
 
 
 class TestEmbedding:
     """Test the embedding function."""
+    def setup_method(self, method):
+        # Replace this with the code to create your DataFrame
+        self.df = Parsing.parse_tsv('tests/head_10.tsv')
+    
     sequences = ['MSLDTIPVVDLGPLLTGD', 'MSLDTIPVVDLGPLLTGD']
 
     @pytest.mark.parametrize("sequences", sequences)
     def test_embeddings(self, sequences): 
         """Test esm-1b embedding"""
         seqs = sequences
-        embeddings = gen_embedding(seqs, device='cuda')
+        embeddings = embed.gen_embedding(seqs, device='cuda')
         
         assert embeddings is not []
-        assert embeddings.shape == (18, 1280)
+        assert embeddings.shape == (18, 1280)  # each of the 18 amino acids is embedded into a 1280 dimensional vector
+
+    def test_gen_embedding(self):
+        """Test esm-1b embedding with a list of sequences."""
+        with pytest.raises(ValueError, match="Sequence length 1630 above maximum sequence length of 1024"):
+            embeddings = embed.gen_embedding(self.df['Sequence'].tolist(), device='cuda')
+            
+    def test_gen_embedding(self):
+        """Test esm-1b embedding with a list of sequences."""
+        pp = Preprocessing(self.df)
+        df = pp.remove_long_sequenes()
+        embeddings = embed.gen_embedding(df['Sequence'].tolist(), device='cuda')
+        assert embeddings is not []
+        assert embeddings.shape == (8, 1280)
+
+
+class TestDBCreation:
+    """Test the embedding function."""
+    def setup_method(self, method):
+        # Replace this with the code to create your DataFrame
+        self.df = Parsing.parse_tsv('tests/head_10.tsv')
+        pp = Preprocessing(self.df)
+        self.df = pp.remove_long_sequenes()
+        self.qdrant = QdrantClient(path="datasets/Vector_db/")  # OR write them to disk
+        self.collections_info = self.qdrant.get_collections()
+        self.collection_name='pytest'
+
+    def test_create_vector_db_collection(self):
+        """Test the creation of a vector database collection."""
+        embeddings = embed.gen_embedding(self.df['Sequence'].tolist(), device='cuda')
+        df, embeddings = embed.create_vector_db_collection(self.df, embeddings, self.collection_name)
+        assert df is not None
+        assert embeddings is not None
+
+    def test_is_DB_created(self):
+        assert self.collection_name in str(self.collections_info)
+
+    def test_load_collection_from_vector_db(self):
+        """Test the loading of an existing vector database collection."""
+        if self.collection_name in str(self.collections_info):  # todo: implement this nicely: access the 'name' field of the object
+            df, embeddings = embed.load_collection_from_vector_db(self.collection_name)
+            assert df is not None
+            assert embeddings is not None
