44801810020740617e93c39ffa7d4d29d0bf1e93
fmoorhof <fmoorhof@ipb-halle.de>
2024-01-22 11:20:43 +0100

    tests: style: objectification

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -1,26 +1,14 @@
 import pytest
 
-from embed import read_fasta
 from embed import gen_embedding
 
 
-def test_reading():
-    # minimal example data set
-    fasta_file = 'head_10.fasta'
+class TestEmbedding:
+    """Test the embedding function."""
 
-    # todo: re-write: this is super ugly -> parse as df directly
-    # get the headers and sequences from the fasta file
-    headers = []
-    sequences = []
-    for h, s in read_fasta(fasta_file):
-        headers.append(h)
-        sequences.append(s)
-
-    assert headers is not []
-    assert sequences is not []
-
-
-def test_embeddings(sequences): 
-    embeddings = gen_embedding(sequences, device='cuda')
-    
-    assert embeddings is not []
+    @pytest.mark.parametrize("sequences", ['MSLDTIPVVDLGPLLTGD', 'MSLDTIPVVDLGPLLTGD'])
+    def test_embeddings(sequences): 
+        """Test esm-1b embedding"""
+        embeddings = gen_embedding(sequences, device='cuda')
+        
+        assert embeddings is not []
