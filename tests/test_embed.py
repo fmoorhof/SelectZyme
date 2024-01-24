1258fd6fe2ade1e030f9f8ca225b09e2b7ebb296
fmoorhof <fmoorhof@ipb-halle.de>
2024-01-24 18:18:44 +0100

    ci: deactivate for branch

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -31,7 +31,8 @@ class TestEmbedding:
     def test_gen_embedding(self):
         """Test esm-1b embedding with a list of sequences."""
         pp = Preprocessing(self.df)
-        df = pp.remove_long_sequenes()
+        pp.remove_long_sequenes()
+        df = pp.df
         embeddings = embed.gen_embedding(df['Sequence'].tolist(), device='cuda')
         assert embeddings is not []
         assert embeddings.shape == (8, 1280)
@@ -43,24 +44,27 @@ class TestDBCreation:
         # Replace this with the code to create your DataFrame
         self.df = Parsing.parse_tsv('tests/head_10.tsv')
         pp = Preprocessing(self.df)
-        self.df = pp.remove_long_sequenes()
+        pp.remove_long_sequenes()
+        self.df = pp.df
+
         self.qdrant = QdrantClient(path="datasets/Vector_db/")  # OR write them to disk
         self.collections_info = self.qdrant.get_collections()
         self.collection_name='pytest'
+        self.embeddings = embed.gen_embedding(self.df['Sequence'].tolist(), device='cuda')
 
     def test_create_vector_db_collection(self):
         """Test the creation of a vector database collection."""
-        embeddings = embed.gen_embedding(self.df['Sequence'].tolist(), device='cuda')
-        df, embeddings = embed.create_vector_db_collection(self.df, embeddings, self.collection_name)
-        assert df is not None
-        assert embeddings is not None
+        embed.create_vector_db_collection(self.qdrant, self.df, self.embeddings, self.collection_name)
 
-    def test_is_DB_created(self):
-        assert self.collection_name in str(self.collections_info)
+        assert self.collection_name in str(self.collections_info)  # is DB created?
+        assert self.collections_info.vectors_count == self.df.shape[0]
 
     def test_load_collection_from_vector_db(self):
         """Test the loading of an existing vector database collection."""
         if self.collection_name in str(self.collections_info):  # todo: implement this nicely: access the 'name' field of the object
-            df, embeddings = embed.load_collection_from_vector_db(self.collection_name)
-            assert df is not None
+            entries, embeddings = embed.load_collection_from_vector_db(self.collection_name)
+
+            assert entries is not None
             assert embeddings is not None
+            assert len(entries) == self.df.shape[0]
+            assert embeddings == self.embeddings
