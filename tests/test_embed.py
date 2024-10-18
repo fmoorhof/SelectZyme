00d28827f1a3e8ada3dd96113b55577ed6deb82c
fmoorhof <fmoorhof@ipb-halle.de>
2024-10-18 04:20:31 +0200

    tests: feat: proper setup and teardown of the qdrant DB connection

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -6,6 +6,22 @@ from preprocessing import Parsing
 from preprocessing import Preprocessing
 
 
+@pytest.fixture(scope="class")
+def setup_and_teardown():
+    """Fixture for setting up and tearing down resources."""
+    # Setup: This code runs before the tests in the class.
+    print("Setting up for the test...")
+    qdrant = QdrantClient(path="datasets/Vector_db/")
+    collection_name = 'pytest'
+
+    yield qdrant, collection_name  # The test functions will run at this point.
+
+    # Teardown: This code runs after the tests in the class.
+    print("Tearing down after the test...")
+    # qdrant.delete_collection(collection_name='pytest')
+    qdrant.close()
+
+
 class TestEmbedding:
     """Test the embedding function."""
     def setup_method(self, method):
@@ -38,50 +54,42 @@ class TestEmbedding:
         assert embeddings.shape == (10, 1280)
 
 
+@pytest.mark.usefixtures("setup_and_teardown")
 class TestDBCreation:
     """Test the embedding function."""
+
+    @pytest.fixture(autouse=True)
     def setup_method(self):
         # Replace this with the code to create your DataFrame
         self.df = Parsing.parse_tsv('tests/head_10.tsv')
         pp = Preprocessing(self.df)
         pp.remove_long_sequences()
         self.df = pp.df
-
-        self.qdrant = QdrantClient(path="datasets/Vector_db/")  # OR write them to disk
-        self.collections_info = self.qdrant.get_collections()
-        self.collection_name = 'pytest'
         self.embeddings = embed.gen_embedding(self.df['sequence'].tolist(), device='cuda')
 
-    def test_create_vector_db_collection(self):
+    def test_create_vector_db_collection(self, setup_and_teardown):
         """Test the creation of a vector database collection."""
-        embed.create_vector_db_collection(self.qdrant, self.df, self.embeddings, self.collection_name)
-        self.collections_info = self.qdrant.get_collections()
+        qdrant, collection_name = setup_and_teardown
+        embed.create_vector_db_collection(qdrant, self.df, self.embeddings, collection_name)
+        collections_info = qdrant.get_collections()
 
-        assert self.collection_name in str(self.collections_info)
-        assert self.collection_name == self.collections_info.collections[1].name
-        self.qdrant.close()  # this is no proper teardown
+        assert collection_name in str(collections_info)
+        assert collection_name == collections_info.collections[0].name
 
-    @pytest.mark.skip(reason="No proper teardown method implemented yet")
-    def test_teardown_method(self):
-        """Teardown method to close DB connection after each test method."""
-        self.qdrant.close()
-
-    # @pytest.mark.skip(reason="No proper teardown method implemented yet")
-    def test_load_collection_from_vector_db(self):
+    def test_load_collection_from_vector_db(self, setup_and_teardown):
         """Test the loading of an existing vector database collection."""
-        if self.collection_name == self.collections_info.collections[1].name:
-            entries, embeddings = embed.load_collection_from_vector_db(self.qdrant, self.collection_name)
+        qdrant, collection_name = setup_and_teardown
+        entries, embeddings = embed.load_collection_from_vector_db(qdrant, collection_name)
 
-            assert entries is not None
-            assert embeddings is not None
-            assert len(entries) == self.df.shape[0]
-            # assert embeddings.tolist() == self.embeddings.tolist()  # todo: fix: why are embeddings not equal!
-            self.qdrant.close()  # this is no proper teardown
+        assert entries is not None
+        assert embeddings is not None
+        assert len(entries) == self.df.shape[0]
 
-    def test_collection_deletion(self):
+    def test_collection_deletion(self, setup_and_teardown):
         """Test the deletion of a collection."""
-        res = self.qdrant.delete_collection(collection_name='pytest')
-        collections_info = self.qdrant.get_collections()
+        qdrant, collection_name = setup_and_teardown
+        res = qdrant.delete_collection(collection_name=collection_name)
+        collections_info = qdrant.get_collections()
 
         assert res is True
-        assert self.collection_name not in str(collections_info)
+        assert collection_name not in str(collections_info)
