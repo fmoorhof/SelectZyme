cdda554d977e4f7b4db2ea7d220db8b6e4a3a65a
fmoorhof <fmoorhof@ipb-halle.de>
2025-01-14 23:24:54 +0100

    feat: fasta file parsing refactoring

--- src/fetch_data_uniprot.py
+++ src/fetch_data_uniprot.py
@@ -12,11 +12,6 @@ import os
 import pandas as pd
 
 
-def sanitize_filename(filename: str) -> str:
-    """Remove any unsafe characters and limit to alphanumerics, underscores, or dashes"""
-    return re.sub(r'[^\w\-.]', '_', filename)
-
-
 class UniProtFetcher:
     def __init__(self, df_coi: list[str], out_dir: str):
         self.df_coi = df_coi
@@ -25,27 +20,44 @@ class UniProtFetcher:
         self.re_next_link = re.compile(r'<(.+)>; rel="next"')
 
     def _init_session(self):
+        """
+        Initializes and returns a new HTTP session with retry logic.
+        The session is configured to retry requests up to 5 times with a backoff factor of 0.25
+        for specific HTTP status codes (500, 502, 503, 504).
+        Returns:
+            Session: A configured requests.Session object.
+        """
+
         retries = Retry(total=5, backoff_factor=0.25, status_forcelist=[500, 502, 503, 504])
         session = Session()
         session.mount("https://", HTTPAdapter(max_retries=retries))
         return session
 
-    def tsv_to_fasta_writer(self, in_file: str, out_file: str):
-        with open(in_file, 'r') as f_in, open(out_file, 'w') as f_out:
-            for line in f_in:
-                l = line.split('\t')
-                if l[0] == 'accession':  # skip header
-                    continue
-                fasta = '>', l[0], '\n', l[-1]  # >Entry, Sequence
-                f_out.writelines(fasta)
-
-    def write_annotated_fasta(self, df: pd.DataFrame, out_file: str):
+    def export_annotated_fasta(self, df: pd.DataFrame, out_file: str):
         with open(out_file, 'w') as f_out:
             for index, row in df.iterrows():
                 fasta = '>', '|'.join(row.iloc[:-1].map(str)), '\n', row.loc["sequence"], '\n'
                 f_out.writelines(fasta)
 
     def load_custom_csv(self, file_path: str, sep: str=';') -> pd.DataFrame:
+        """
+        Load a custom CSV file into a pandas DataFrame.
+        This function reads a CSV file from the specified file path, processes the data,
+        and returns it as a pandas DataFrame. The CSV file is expected to have specific
+        columns such as 'sequence', 'xref_brenda', and 'ec'.
+        Args:
+            file_path (str): The path to the CSV file to be loaded.
+            sep (str, optional): The delimiter to use for separating values in the CSV file. Defaults to ';'.
+        Returns:
+            pd.DataFrame: A pandas DataFrame containing the processed data from the CSV file.
+        Raises:
+            Exception: If there is an error loading the CSV file, an exception is raised with an error message.
+        Notes:
+            - The 'sequence' column values are converted to uppercase.
+            - Any '<BR>' substrings in the 'sequence' column are removed. Useful for data reupload after using the tool.
+            - If the 'xref_brenda' column contains only null values, it is set to True.
+            - If the 'ec' column contains only null values, it is set to True.
+        """
         try:
             df = pd.read_csv(file_path, sep=sep)
             df['reviewed'] = True
@@ -61,20 +73,45 @@ class UniProtFetcher:
             logging.error("Please check the custom data file provided and ensure it follows the template, avoiding characters such as tabs or ';'")
             raise
 
-    def get_next_link(self, headers):
-        if "Link" in headers:
-            if match := self.re_next_link.match(headers["Link"]):
-                return match.group(1)
-
-    def get_batch(self, batch_url: str):
-        while batch_url:
-            response = self.session.get(batch_url)
-            response.raise_for_status()
-            total = response.headers["x-total-results"]
-            yield response, total
-            batch_url = self.get_next_link(headers=response.headers)
+    def load_custom_fasta(self, file_path: str) -> pd.DataFrame:
+        """
+        Load sequences from a custom FASTA file into a pandas DataFrame.
+        This method reads a FASTA file, extracts the headers and sequences, and 
+        returns them in a pandas DataFrame with two columns: 'accession' and 'sequence'.
+        Args:
+            file_path (str): The path to the FASTA file to be loaded.
+        Returns:
+            pd.DataFrame: A DataFrame containing the headers and sequences from the FASTA file.
+                          The 'accession' column contains the headers (without the '>' character),
+                          and the 'sequence' column contains the sequences.
+        Raises:
+            Exception: If there is an error reading the file or processing its contents.
+        """
+        try:
+            with open(file_path, 'r') as f:
+                lines = f.readlines()
+            headers = [line for line in lines if line.startswith('>')]
+            sequences = [line for line in lines if not line.startswith('>')]
+            df = pd.DataFrame({'accession': headers, 'sequence': sequences})
+            df['accession'] = df['accession'].str.strip('>').str.strip()  # todo: add a field separator later if needed and create (noname?) columns
+            df['sequence'] = df['sequence'].str.strip()
+            return df
+        except Exception as e:
+            logging.error(f"Error loading custom FASTA file: {e}")
+            logging.error("Please check the custom data file provided and ensure it follows the FASTA format")
+            raise
 
     def query_uniprot(self, query_terms: list[str], length: int) -> pd.DataFrame:
+        """
+        Queries the UniProt database with the given query terms and sequence length, and returns the results as a pandas DataFrame.
+        Args:
+            query_terms (list[str]): A list of query terms to search for in the UniProt database.
+            length (int): The length of the protein sequences to filter by.
+        Returns:
+            pd.DataFrame: A DataFrame containing the query results with columns specified in self.df_coi. The 'reviewed' column is a boolean indicating whether the entry is reviewed.
+        Raises:
+            ValueError: If the query to UniProt fails or returns an error.
+        """
         coi = str(self.df_coi).strip('[]').replace("'", "")
         raw_data = b''  # byte variable initialization
         for qry in query_terms:
@@ -84,7 +121,7 @@ class UniProtFetcher:
                   f"&fields={coi}" \
                   f"&size=500"  # UniProt pagination to fetch more than 500 entries
 
-            for batch, total in self.get_batch(batch_url=url):
+            for batch, total in self._get_batch(batch_url=url):
                 raw_data += batch.content  # append two bytes in python
 
         df = pd.read_csv(io.StringIO(raw_data.decode('utf-8')), delimiter='\t')
@@ -97,6 +134,10 @@ class UniProtFetcher:
         logging.info(f'Total amount of retrieved entries: {df.shape[0]}')
         df.drop_duplicates(subset='accession', keep='first', inplace=True)
         df.reset_index(drop=True, inplace=True)
+        # Move 'sequence' column to the end (to be avoided for .fasta export)
+        sequence_col = df.pop('sequence')
+        df['sequence'] = sequence_col
+
         logging.info(f'Total amount of non redundant entries: {df.shape[0]}')
         logging.info(f"Amount of BRENDA reviewed entries: {df['xref_brenda'].notna().sum()}")
         return df
@@ -118,11 +159,29 @@ class UniProtFetcher:
         
         out_filename = sanitize_filename(out_filename)
         df.to_csv(os.path.join(self.out_dir, out_filename + "_annotated.tsv"), sep='\t', index=False)
-        self.tsv_to_fasta_writer(in_file=os.path.join(self.out_dir, out_filename + "_annotated.tsv"), out_file=os.path.join(self.out_dir, out_filename + '.fasta'))
-        self.write_annotated_fasta(df=df, out_file=os.path.join(self.out_dir, out_filename + '_annotated.fasta'))
+        self.export_annotated_fasta(df=df, out_file=os.path.join(self.out_dir, out_filename + '_annotated.fasta'))
 
         df = df[(df['reviewed'] == True) | (df['xref_brenda'].notnull())]  # | = OR
-        df.to_csv(os.path.join(self.out_dir + out_filename + '_BRENDA.tsv'), sep='\t', index=False)
+        df.to_csv(os.path.join(self.out_dir + out_filename + '_BRENDA_only.tsv'), sep='\t', index=False)
+
+    def _get_next_link(self, headers):
+        if "Link" in headers:
+            if match := self.re_next_link.match(headers["Link"]):
+                return match.group(1)
+
+    def _get_batch(self, batch_url: str):
+        while batch_url:
+            response = self.session.get(batch_url)
+            response.raise_for_status()
+            total = response.headers["x-total-results"]
+            yield response, total
+            batch_url = self._get_next_link(headers=response.headers)
+
+
+def sanitize_filename(filename: str) -> str:
+    """Remove any unsafe characters and limit to alphanumerics, underscores, or dashes"""
+    return re.sub(r'[^\w\-.]', '_', filename)
+
 
 
 if __name__ == '__main__':
@@ -156,18 +215,21 @@ if __name__ == '__main__':
     out_filename = "uniprot_PapE"
 
     # minimal test
-    query_terms = ["ec:3.2.1.64", "PF01771"]
+    query_terms = ["ec:3.2.1.64"]  #  , "PF01771"]
     length = "200 TO 1020"
-    custom_data_location = '/raid/data/fmoorhof/PhD/SideShit/PapE/custom_seqs.csv'
+    custom_data_location = '/raid/data/fmoorhof/PhD/Data/SKD021_Case_studies/PETase/PlasticDB.fasta'
     out_dir = 'datasets/output/'  # describe desired output location
     out_filename = "test_data"
 
     fetcher = UniProtFetcher(df_coi, out_dir)
 
-    # Retrieve and save data for lcp
+    # Retrieve and save data
     df = fetcher.query_uniprot(query_terms, length)
     if custom_data_location != '':
-        custom_data = fetcher.load_custom_csv(custom_data_location)
+        if custom_data_location.endswith('.fasta'):
+            custom_data = fetcher.load_custom_fasta(custom_data_location)
+        else:
+            custom_data = fetcher.load_custom_csv(custom_data_location)
         df = pd.concat([custom_data, df], ignore_index=True)
     df = fetcher.clean_data(df)
     fetcher.save_data(df, out_filename)
