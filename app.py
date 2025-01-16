f188cb1fe4845447b8b0a7a0956ab8f4d265658d
fmoorhof <fmoorhof@ipb-halle.de>
2025-01-16 23:46:37 +0100

    layout: DimRed as page with working callbacks

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-"""todo: make landing page here and then reference the different pages. somehow share the table components across tables (possible?)
-"""
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 import networkx as nx
+
 # development only
 import argparse
 from src.main import parse_data, preprocessing, database_access, dimred_clust
@@ -37,11 +36,14 @@ X = database_access(df, args.project_name)
 df, X_red, G, Gsl = dimred_clust(df, X, args.dim_red)
 
 
-dash.register_page('mst', name="Minimal Spanning Tree", layout=mst.layout(G, df, X_red))
-dash.register_page('single-linkage', name="Phylogenetic Tree", layout=sl.layout(G=Gsl, df=df, polar=False))  # todo: parse here truncation_mode and p
-# dash.register_page('dim', name="Dimensionality reduction and clustering", layout=dimred.layout(df, X_red, 'TSNE', 'test'))
-dimred_layout, dimred_register_callbacks = dimred.layout(df, X_red)
-dimred_register_callbacks(app)  # Call the register_callbacks function to register the callbacks
+# dash.register_page('mst', name="Minimal Spanning Tree", layout=mst.layout(G, df, X_red))
+# dash.register_page('single-linkage', name="Phylogenetic Tree", layout=sl.layout(G=Gsl, df=df, polar=False))  # todo: parse here truncation_mode and p
+dash.register_page('dim', name="Dimensionality reduction and clustering", layout=dimred.layout(df, X_red))
+# dimred_layout, dimred_register_callbacks = dimred.layout(df, X_red)
+# dimred_register_callbacks(app)  # Call the register_callbacks function to register the callbacks
+
+# Register callbacks for the dimensionality reduction page
+dimred.register_callbacks(app, df, X_red)
 
 # Layout with navigation links and page container
 app.layout = dbc.Container(
@@ -64,7 +66,7 @@ app.layout = dbc.Container(
                 ),
                 html.Hr(),
                 dash.page_container,  # Displays the content of the current page
-                dimred_layout  # insert dimensionality reduction layout from here
+                # dimred_layout  # insert dimensionality reduction layout from here
             ]
         ),
     ],
