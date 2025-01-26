d9b2984d72e275ce43fc3bf12430cb82d1452a4c
fmoorhof <fmoorhof@ipb-halle.de>
2025-01-26 15:16:06 +0100

    feat: SLC: nodes in phylogenetic tree + with properties from other plots (size, shape, color), color lines if selected == True [prepared legend_attribute that color can be adapted with dropdown and callback if desired]

--- src/single_linkage_plotting.py
+++ src/single_linkage_plotting.py
@@ -5,24 +5,35 @@ but removed all additional functionalities and adapted template to my needs.
 import numpy as np
 from scipy.cluster.hierarchy import dendrogram
 import plotly.graph_objects as go
+import plotly.colors as pc
 
-def create_dendrogram(Z, df, hovertext=None):
+
+def create_dendrogram(Z, df, hovertext=None, legend_attribute: str = 'cluster'):
     P = dendrogram(Z, no_plot=True)
     icoord = np.array(P["icoord"])
     dcoord = np.array(P["dcoord"])
 
+    # Get the color mapping and apply it to the DataFrame
+    color_mapping = _value_to_color(df[legend_attribute])
+    df['cluster_colors'] = df[legend_attribute].map(color_mapping)
+
     # Create traces for the dendrogram
     traces = []
     for i in range(len(icoord)):
-        text = hovertext[i] if hovertext else None
-
         trace = go.Scattergl(
             x=icoord[i],
             y=dcoord[i],
-            mode="lines",
-            line=dict(color="black"),
+            # name=str(legend_attribute),  # Legend name
+            mode="lines+markers",
+            line=dict(color="red" if df['selected'][i] == True else "black"),
+            marker=dict(
+                size=df['marker_size'][i],
+                symbol=df['marker_symbol'][i],
+                color=df['cluster_colors'][i],
+                opacity=0.8
+            ),
             customdata=df['accession'],
-            text=text,
+            text=hovertext[i],
             hoverinfo="text",
         )
         traces.append(trace)
@@ -30,11 +41,39 @@ def create_dendrogram(Z, df, hovertext=None):
     fig = go.Figure(data=traces)
     fig.update_layout(
         xaxis_title="Cluster/Variant",
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
         yaxis_title="Distance",
         showlegend=False,
+        # legend_title_text=legend_attribute
     )
 
     return fig
+
+
+def _value_to_color(values):
+    """
+    Maps a list of values to a continuous color scale.
+
+    Parameters:
+    -----------
+    values : array-like
+        Values to map to a color scale.
+    Returns:
+    --------
+    color_mapping : dict
+        A dictionary mapping unique values to corresponding colors in the colormap.
+    """
+    # Normalize the values to [0, 1]
+    unique_values = np.unique(values)
+    norm = (unique_values - unique_values.min()) / (unique_values.max() - unique_values.min())
+    
+    # Get the Plotly colormap
+    colorscale = pc.get_colorscale('Viridis')
+    colormap_func = pc.sample_colorscale(colorscale, norm, low=0, high=1)
+    
+    # Map each unique value to a color
+    color_mapping = dict(zip(unique_values, colormap_func))
+    return color_mapping
 
 
 
@@ -54,8 +93,8 @@ if __name__ == "__main__":
     df = pd.DataFrame({
         'x': np.random.randn(sample_size),
         'y': np.random.randn(sample_size),
-        'accession': np.random.choice(['A', 'B', 'C'], sample_size),
-        'cluster': np.random.choice(['A', 'B', 'C'], sample_size),
+        'accession': np.arange(sample_size),
+        'cluster': np.random.choice([-1, 1, 3], sample_size),
         'selected': np.random.choice([False, True], sample_size),
         'marker_symbol': np.random.choice(['circle', 'square', 'diamond', 'triangle-up'], sample_size),
         'marker_size': np.random.randint(10, sample_size, sample_size)
