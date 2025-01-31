17d0b822d97c689e8006f1c8a675d0b6dfbe0c6b
fmoorhof <fmoorhof@ipb-halle.de>
2025-01-31 10:04:31 +0100

    revert: fix: deprecated collection.vectors_count can be collection.points_count but is also not needed i guess !is needed, else only 10 points are loaded and cause collection to be deleted

--- src/vector_db.py
+++ src/vector_db.py
@@ -57,7 +57,8 @@ def load_collection_from_vector_db(qdrant, collection_name: str) -> list:
     collection = qdrant.get_collection(collection_name)
     records = qdrant.scroll(collection_name=collection_name,
                             with_payload=True,  # If List of string - include only specified fields
-                            with_vectors=True)
+                            with_vectors=True,
+                            limit=collection.points_count)
     # qdrant.delete_collection(collection_name)
 
     # extract the header and vector from the Qdrant data structure
