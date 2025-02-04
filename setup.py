14f2ac4c479ea10c6920e8d1dc46bed0e6d16ece
fmoorhof <fmoorhof@ipb-halle.de>
2025-02-04 09:56:40 +0100

    deployment: fix: setup.py conflicts pyproject.toml but pyproject.toml doesnt install project so setup.py is required WIP: assert why pip install -e . uses toml which is not working [workaround: delete pyproject.toml and pip install -e .]

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ec",
+    version="0.0.1",
+    author="Felix Moorhoff",
+    author_email="fmoorhof@ipb-halle.de",
+    url="https://www.ipb-halle.de/en/research/bioorganic-chemistry/research-groups/computational-chemistry/projects/",
+    description="Selectzyme: Interactive Protein Space Visualization for Enzyme Discovery, Selection, and Mining",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": ["ec = src.main:main"]},  # very important to make local imports work!
+)
