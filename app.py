2712b2c9a954eb2946f6430a3d8c225fc09e7957
fmoorhof <fmoorhof@ipb-halle.de>
2025-02-13 09:50:52 +0100

    clean old jobs and comments

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ def main(app):
 
 
     # Create page layouts
-    dash.register_page('eda', name="Explanatory data analysis", layout=eda.layout(df))
-    dash.register_page('dim', name="Dimensionality reduction and clustering", layout=dimred.layout(df, X_red, X_red_centroids))
-    dash.register_page('sl', name="Phylogenetic Tree", layout=sl.layout(G=Gsl, df=df))    
+    dash.register_page('eda', name="Explanatory Data Analysis", layout=eda.layout(df))
+    dash.register_page('dim', name="Dimensionality Reduction and Clustering", layout=dimred.layout(df, X_red, X_red_centroids))
+    dash.register_page('slc', name="Phylogenetic Tree", layout=sl.layout(G=Gsl, df=df))    
     dash.register_page('mst', name="Minimal Spanning Tree", layout=mst.layout(G, df, X_red))
 
     # Register callbacks
@@ -96,27 +96,11 @@ if __name__ == "__main__":
     )
     # server = app.server  # this line is only needed when deployed on a (public) server
 
-    # load real minimal data
-    # args = argparse.Namespace(project_name='argparse_test_minimal', query_terms=["ec:1.13.11.85", "ec:1.13.11.84"], length='200 TO 601', custom_data_location="/raid/data/fmoorhof/PhD/SideShit/LCP/custom_seqs_no_signals.csv", dim_red='PCA', plm_model='prott5', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-    # args = argparse.Namespace(project_name='argparse_test', query_terms=["ec:1.13.11.85", "latex clearing protein"], length='200 TO 601', custom_data_location="/raid/data/fmoorhof/PhD/SideShit/LCP/custom_seqs_no_signals.csv", dim_red='PCA', plm_model='esm1b', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-    # args = argparse.Namespace(project_name='batch7', query_terms=["ec:1.14.11", "ec:1.14.20","xref%3Abrenda-1.14.11", "xref%3Abrenda-1.14.20", "IPR005123", "IPR003819", "IPR026992", "PF03171", "2OG-FeII_Oxy", "cd00250"], length='201 TO 500', custom_data_location="/raid/data/fmoorhof/PhD/Data/SKD022_2nd-order/custom_seqs_full.csv", dim_red='TSNE', plm_model='esm1b', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-    # args = argparse.Namespace(project_name='lefos', query_terms=["ec:1.13.11.85", "ec:1.13.11.84"], length='200 TO 601', custom_data_location="/raid/data/fmoorhof/PhD/SideShit/LeFOS/custom_seqs.csv", dim_red='TSNE', plm_model='esm1b', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-    # args = argparse.Namespace(project_name='lefos_prostt5', query_terms=["ec:1.13.11.85", "ec:1.13.11.84"], length='200 TO 601', custom_data_location="/raid/data/fmoorhof/PhD/SideShit/LeFOS/custom_seqs.csv", dim_red='TSNE', plm_model='prostt5', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-    
-    # args = argparse.Namespace(project_name='PapE', query_terms=["IPR001616", "IPR034720", "PF01771", "IPR011335"], length='350 TO 600', custom_data_location="/raid/data/fmoorhof/PhD/SideShit/PapE/custom_seqs.csv", dim_red='umap', plm_model='prott5', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-    # args = argparse.Namespace(project_name='PapEC', query_terms=["IPR001616", "IPR034720", "PF01771"], length='350 TO 600', custom_data_location="/raid/data/fmoorhof/PhD/SideShit/PapE/custom_seqs.csv", dim_red='PCA', plm_model='prott5', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-
-    # query_terms = ["ec:1.13.11.85", "xref%3Abrenda-1.13.11.85", "ec:1.13.11.87", "xref%3Abrenda-1.13.11.87", "ec:1.13.99.B1", "xref%3Abrenda-1.13.99.B1", "IPR037473", "IPR018713", "latex clearing protein"]  # define your query terms for UniProt here
-    # args = argparse.Namespace(project_name='petase', query_terms=query_terms, length='50 TO 1020', custom_data_location='/raid/data/fmoorhof/PhD/Data/SKD021_Case_studies/PETase/pet_plasticDB_preprocessed.csv', dim_red='TSNE', plm_model='esm1b', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-    # args = argparse.Namespace(project_name='petase2', query_terms=query_terms, length='50 TO 1020', custom_data_location='/raid/data/fmoorhof/PhD/Data/SKD021_Case_studies/PETase/pet_plasticDB_preprocessed_final.csv', dim_red='TSNE', plm_model='esm1b', out_dir='datasets/output/', df_coi=['accession', 'reviewed', 'ec', 'organism_id', 'length', 'xref_brenda', 'xref_pdb', 'sequence'])
-    # args = parse_args()
-
-    # new argparsing implementation
-    # CLI way
+    # CLI argument parsing
     config = parse_args()
-    # Debugging way
+    # Debugging way, only runs always the test_config.yml
     import yaml
-    args = argparse.Namespace(config='results/upo.yml')
+    args = argparse.Namespace(config='results/test_config.yml')
     with open(args.config, 'r') as f:
             config = yaml.safe_load(f)
 
